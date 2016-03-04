f7ebda23c169e3f0565b27aeb21b9c25bc5c2454
Ellery Queen <yhmsc007@hotmail.com>
2016-03-04 23:24:38 +0800

    Item enum

--- src/com/kururu/Item.java
+++ src/com/kururu/Item.java
@@ -5,4 +5,31 @@ package com.kururu;
  */
 public enum Item {
 
+    ITEM000001("badminto",false),
+    ITEM000002("cpp_primer_5th",false),
+    ITEM000003("apple",true),
+    ITEM000004("banana",true),
+    ITEM000005("cola",false);
+
+    private String itemName;
+    private boolean isWeigh;
+
+    Item(String itemName,boolean isWeigh){
+        this.itemName = itemName;
+        this.isWeigh = isWeigh;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public boolean isWeigh() {
+        return isWeigh;
+    }
+
+    @Override
+    public String toString() {
+        return "itemName:" + itemName + "isWeigh" + isWeigh;
+    }
 }
+
