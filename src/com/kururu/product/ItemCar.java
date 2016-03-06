e795252e8d94661427634bf22ad745f8a9f2753c
Ellery Queen <yhmsc007@hotmail.com>
2016-03-06 23:22:41 +0800

    item and shopping car

---
+++ src/com/kururu/product/ItemCar.java
@@ -0,0 +1,14 @@
+package com.kururu.product;
+
+import java.util.List;
+
+/**
+ * Created by Ellery Queen on 2016/3/6.
+ */
+public class ItemCar {
+
+    private List<NewItem> shoppingCar;
+    private double total;
+
+
+}
--- src/com/kururu/product/NewItem.java
+++ src/com/kururu/product/NewItem.java
@@ -4,4 +4,59 @@ package com.kururu.product;
  * Created by Ellery Queen on 2016/3/6.
  */
 public class NewItem {
+
+    //get from ticket
+    private String goodsID;
+    private int amount;
+
+    //get from SQL
+    private double unitPrice;
+    private String goodsName;
+    private boolean isDiscountNintyFivePercents;
+    private boolean isDoubleByOne;
+
+    public NewItem(String goodsID, int amount) {
+        this.goodsID = goodsID;
+        this.amount = amount;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public void setDiscountNintyFivePercents(boolean discountNintyFivePercents) {
+        isDiscountNintyFivePercents = discountNintyFivePercents;
+    }
+
+    public void setDoubleByOne(boolean doubleByOne) {
+        isDoubleByOne = doubleByOne;
+    }
+
+    public String getGoodsID() {
+        return goodsID;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public boolean isDiscountNintyFivePercents() {
+        return isDiscountNintyFivePercents;
+    }
+
+    public boolean isDoubleByOne() {
+        return isDoubleByOne;
+    }
 }
